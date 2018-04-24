621b537dc2e975166b53c88021fa22cdb192a688
haasad <haasad@student.ethz.ch>
2018-04-24 17:14:27 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='eidl',
+    packages=['eidl'],
+    version='1.0.0',
+    author="Adrian Haas",
+    url="https://github.com/haasad/EcoInventDownLoader",
+    license=open('LICENSE').read(),
+    description='Download, unpack and import ecoinvent into your brightway2 project in one simple step',
+    long_description=open('README.md').read()
+)
