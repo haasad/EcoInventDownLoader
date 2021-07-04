1f2d909aa1d44f0bba192808c896ced929bf16cf
haasad <haasad@users.noreply.github.com>
2021-07-04 15:49:31 +0200

    Bump to v1.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='eidl',
     packages=['eidl'],
-    version='1.3.0',
+    version='1.3.1',
     author="Adrian Haas",
     url="https://github.com/haasad/EcoInventDownLoader",
     license=open('LICENSE').read(),
