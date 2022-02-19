88d5fe677e93f4fe05c30403a90c9c61fb7d746f
haasad <haasad@users.noreply.github.com>
2022-02-19 08:26:36 +0100

    Get version from git tag env variable used during build

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+import os
 from setuptools import setup
 
 setup(
     name='eidl',
     packages=['eidl'],
-    version='1.4.0',
+    version=os.environ.get('GIT_DESCRIBE_TAG', '0.0.0'),
     author="Adrian Haas",
     url="https://github.com/haasad/EcoInventDownLoader",
     license=open('LICENSE').read(),
